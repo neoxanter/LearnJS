88255a80536387c14e1be971af646cc6a439d706
Serg Mark <sgcont@gmail.com>
2022-07-03 11:44:40 +0300

    Added and modified methods

--- script_films/js/script.js
+++ script_films/js/script.js
@@ -51,8 +51,26 @@ const personalMovieDB = {
     for (let i = 1; i <= 3; i++) {
       // const genre = prompt(`Ваш любимый жанр под номером ${i}`);
       // personalMovieDB.genres[i - 1] = genre; // для начала массива с индекса 0
-      
-      personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+      let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+      if(genre == '' || genre == null) {
+        console.log('Вы ввели некорректные данные или не ввели их сщвсем');
+        i--;
+      } else {
+        personalMovieDB.genres[i - 1] = genre;
+      }   
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+        console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
+
+  },
+
+  toggleVisibleMyDB: function() {
+    if(personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
     }
   }
 };
