8d3bf94d9be6de1fa72f39d91a8201cfd9850ed7
Serg Mark <sgcont@gmail.com>
2022-11-09 18:30:39 +0300

    Created a randomly generated image of the first die

---
+++ dice_dom/index.js
@@ -0,0 +1,8 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1
+
+let randomDiceImage = 'dice' + randomNumber1 + 'png'
+
+let randomImageSource = 'images/' + randomDiceImage
+
+let image1 = document.querySelectorAll('img')[0]
+image1.setAttribute('src', randomImageSource)
