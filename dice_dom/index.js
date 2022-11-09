a1857e204f8e2864045a482e7ad3b7540dddec0b
Serg Mark <sgcont@gmail.com>
2022-11-09 18:42:05 +0300

    Finished with the generation of both cubes

--- dice_dom/index.js
+++ dice_dom/index.js
@@ -1,8 +1,14 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1
 
-let randomDiceImage = 'dice' + randomNumber1 + 'png'
+let randomDiceImage = 'dice' + randomNumber1 + '.png'
 
-let randomImageSource = 'images/' + randomDiceImage
+let randomImageSource1 = 'images/' + randomDiceImage
 
 let image1 = document.querySelectorAll('img')[0]
-image1.setAttribute('src', randomImageSource)
+image1.setAttribute('src', randomImageSource1)
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1
+
+let randomImageSource2 = 'images/dice' + randomNumber2 + '.png'
+
+document.querySelectorAll('img')[1].setAttribute('src', randomImageSource2)
